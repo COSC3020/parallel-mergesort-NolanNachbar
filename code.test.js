b4e8b53786f69c9ca92c27700b889f7a4485701c
NolanNachbar <112431921+NolanNachbar@users.noreply.github.com>
2024-12-05 11:34:08 -0700

    Update code.test.js

--- code.test.js
+++ code.test.js
@@ -3,19 +3,23 @@ const assert = require('assert');
 
 eval(fs.readFileSync('code.js')+'');
 
+function arraysEqual(arr1, arr2) {
+    return JSON.stringify(arr1) === JSON.stringify(arr2);
+}
+
 async function test() {
   let key = 1; 
   
   arr = [];
   await new Promise((resolve) => mergesort(arr, resolve));
-  assert(arr == []);
+  assert(arraysEqual(arr, []));
 
   arr = [1, 2, 1];
   await new Promise((resolve) => mergesort(arr, resolve));
-  assert(arr == [1, 1, 2]);
+  assert(arraysEqual(arr, [1, 1, 2]));
   
   arr = [1, 2, 3, 1, 6, 7, 1];
   await new Promise((resolve) => mergesort(arr, resolve));
-  assert(arr == [1, 1, 1, 6, 2, 3, 7 ]);
+  assert(arraysEqual(arr, [1, 1, 1, 6, 2, 3, 7 ]));
 }
 test();
