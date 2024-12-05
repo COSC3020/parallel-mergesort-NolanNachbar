e368b910023f7856d76c3874409b2cab0968b1bd
NolanNachbar <112431921+NolanNachbar@users.noreply.github.com>
2024-12-05 11:38:48 -0700

    Update code.test.js

--- code.test.js
+++ code.test.js
@@ -8,18 +8,19 @@ function arraysEqual(arr1, arr2) {
 }
 
 async function test() {
-  let key = 1; 
-  
+  let arr;
+
   arr = [];
   await new Promise((resolve) => mergesort(arr, resolve));
-  assert(arraysEqual(arr, []));
+  assert(arraysEqual(arr, []), `Expected [] but got ${arr}`);
 
   arr = [1, 2, 1];
   await new Promise((resolve) => mergesort(arr, resolve));
-  assert(arraysEqual(arr, [1, 1, 2]));
+  assert(arraysEqual(arr, [1, 1, 2]), `Expected [1, 1, 2] but got ${arr}`);
   
   arr = [1, 2, 3, 1, 6, 7, 1];
   await new Promise((resolve) => mergesort(arr, resolve));
-  assert(arraysEqual(arr, [1, 1, 1, 6, 2, 3, 7 ]));
+  assert(arraysEqual(arr, [1, 1, 1, 2, 3, 6, 7]), `Expected [1, 1, 1, 2, 3, 6, 7] but got ${arr}`);
 }
-test();
+
+test()
