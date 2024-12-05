ec7db9452f5d7b2b06eeaf7ebe78d60899224dea
NolanNachbar <112431921+NolanNachbar@users.noreply.github.com>
2024-12-05 11:31:18 -0700

    Update code.js

--- code.js
+++ code.js
@@ -1,28 +1,40 @@
+const async = require('async');
 
-function mergesort(array) {
-    var subarraysize = 1;
+function msort(x, lo, hi, tmp, callback) {
+  if (lo >= hi) {
+    callback();
+    return;
+  }
 
-    while (subarraysize < array.length) {
-        for (var start = 0; start < array.length; start += 2 * subarraysize) {
-            var midsub = Math.min(start + subarraysize, array.length);
-            var endsub = Math.min(start + 2 * subarraysize, array.length);
-            
-            var a = start, b = midsub;
-            while (a < b && b < endsub) {
-                if (array[a] <= array[b]) {
-                    a++;
-                } else {
-                    let t = array[b];
-                    for (let k = b; k > a; k--) {
-                        array[k] = array[k - 1];
-                    }
-                    array[a] = t;
-                    a++;
-                    b++;
-                }
-            }
-        }
-        subarraysize *= 2;
+  var mid = Math.floor((lo + hi) / 2);
+
+  async.parallel(
+    [
+      (done) => msort(x, lo, mid, tmp, done), // left half
+      (done) => msort(x, mid + 1, hi, tmp, done), // right half
+    ],
+      () => {
+      merge(x, lo, mid, hi, tmp);
+      callback();
     }
-    return array;
+  );
+}
+
+function mergesort(x, callback) {
+  const tmp = [];
+  msort(x, 0, x.length - 1, tmp, callback);
+}
+
+function merge(x, lo, mid, hi, tmp) {
+  let a = lo, b = mid + 1;
+  for (let k = lo; k <= hi; k++) {
+    if (a <= mid && (b > hi || x[a] <= x[b])) {
+      tmp[k] = x[a++];
+    } else {
+      tmp[k] = x[b++];
+    }
+  }
+  for (let k = lo; k <= hi; k++) {
+    x[k] = tmp[k];
+  }
 }
